9ae06a2e096861832cc9b6f5a438a926eff029e4
Nate Holler <n8holler10@gmail.com>
2025-01-13 21:54:50 -0500

    added maxDPS and totalDPS to the ModPlayer class and set up variable resets for each bossfight, we need to add output but the basic backend is set up

--- bossStatTracker.cs
+++ bossStatTracker.cs
@@ -18,44 +18,85 @@ namespace bossStatTracker
 	public class bossStatPlayer : ModPlayer{
 		//individual player based configs
 
-		private int bossDamageDealtThisSecond = 0; // Damage dealt in the current second
+		private int bossDmgDealtThisFrame = 0; // Damage dealt in the current second
         private int bossDps = 0; // DPS value
 		private bool isBossActive = false;
         private int bossFightTimer = 0;
+		private int frameCounter=0;
+		private int maxDmgPerSec =0;
+		private int totalDamage=0;
 
-        public override void PreUpdate(){
+
+
+        public override void PostUpdate(){
 			
             bool bossFound = false;
+			frameCounter++; //tick the frame counter up, this runs at the end of every frame
+
+			//reset DPS calculations every second (60fps)
+			if (frameCounter>=60){
+				bossDps=bossDmgDealtThisFrame; //store the value of the 60 frames worth of damage and reset 
+				bossDmgDealtThisFrame=0;
+				frameCounter=0;
+
+				//send the dps over to maxDPS for calculations
+				maxDPS(bossDps);
+			}
 
             // Check if any boss is active
             foreach (var npc in Main.npc){
-                if (npc.active && npc.boss){ // NPC is active and is a boss
+                if (npc.active && npc.boss){ // NPC is active (Alive) and is a boss
                     bossFound = true;
                     break;
                 }
-            }
-			if (bossFound){
-				if(!isBossActive){
+			}
+
+			if (bossFound){ //if the boss is alive
+				if(!isBossActive){ //tells it this is the beginning since we reset the variable below
 					//fight has started
 					isBossActive=true;
-					bossFightTimer=0; //reset timer
+
+					//set all global variables to 0
+					bossFightTimer=0;
+					frameCounter=0;
+					bossDps=0;
+					bossDmgDealtThisFrame=0;
+					maxDmgPerSec=0;
+					totalDamage=0;
+
 				}
 				bossFightTimer++;
 			}else{
-				if (isBossActive){
+				if (isBossActive){ //if the boss is no longer alive
 					//fight has ended
 					isBossActive=false;
 				}
 			}
 		}
 
+
+
  		// Regular OnHitNPC (accounts for all weapon types)
 		public override void OnHitNPC( NPC target, NPC.HitInfo hitInfo, int damage){
             // Only track damage if the target is a boss
             if (target.active && target.boss && damage > 0){
-                bossDamageDealtThisSecond += damage;
+                bossDmgDealtThisFrame += damage;
             }
         }
 
+
+
+		//keeps track of the highest dps during the boss fight
+		public void maxDPS(int bossDps){
+			int tempDps= bossDps;
+			if(tempDps > maxDmgPerSec){maxDmgPerSec = tempDps;}
+		}
+
+
+
+		//adding up each dps for the entirety of the battle
+		public void totalDPS(int bossDps){
+			totalDamage+=bossDps;
+		}
 	}
 }
--- obj/Debug/net8.0/bossStatTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/bossStatTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bossStatTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abf1226945c4d66c08a874594851058f769ad5e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e64b5b598c06330023e78d9576cfe7008440be9")]
 [assembly: System.Reflection.AssemblyProductAttribute("bossStatTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bossStatTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
